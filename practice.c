73f0a213ac89a321febbd3f27922ca10cefefd07
Md Jonayet Hossain <hossainobiick@gmail.com>
2022-11-12 16:38:06 -0500

    hello lets see +

---
+++ practice.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define size 100 
+int main()
+{
+    char name[size] ; 
+    int alp, digit, spec, i ; 
+    printf("print a name"); 
+    fgets(name, sizeof(size), stdin) ; 
+}
